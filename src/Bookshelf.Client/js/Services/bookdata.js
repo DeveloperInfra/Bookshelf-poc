767c8a90defeacc32c5146316161dce419ceeaa1
Mark A. Wilson <markwilson@live.com>
2013-08-18 21:32:59 -0400

    Update Windows Store App (Client)
    For the most part, the client side should be complete. Just need to test
    it and see if it works.

---
+++ src/Bookshelf.Client/js/Services/bookdata.js
@@ -0,0 +1,62 @@
+﻿/**
+ * A book service for working with book data.
+ *
+ * Ideally this would use something like ng-resource
+ * to make restful calls to an api for all of these
+ * operations. 
+ *
+ * @author James Huston <james@jameshuston.net>
+ * @since 2013-07-18
+ */
+angular.module('bookshelf.services.books', [])
+    .factory('bookService', [
+        '$http',
+        function ($http) {
+            return {
+                getBooks: function (bookId) {
+                    if (bookId === undefined) {
+                        return $http({
+                            method: 'GET',
+                            url: 'http://localhost:1501/api/book'
+                        });
+                    }
+
+                    bookId = parseInt(bookId);
+
+                    return $http({
+                        method: 'GET',
+                        url: 'http://localhost:1501/api/book/' + bookId
+                    });
+                },
+
+                addBook: function (bookData) {
+                    return $http({
+                        method: 'POST',
+                        url: 'http://localhost:1501/api/book',
+                        data: bookData
+                    });
+                },
+
+                updateBook: function (bookId, bookData) {
+                    return $http({
+                        method: 'PUT',
+                        url: 'http://localhost:1501/api/book/' + bookId,
+                        data: bookData
+                    });
+                },
+
+                deleteBook: function (bookId) {
+                    if (bookId === undefined) {
+                        bookId = -1;
+                    }
+
+                    bookId = parseInt(bookId);
+
+                    return $http({
+                        method: 'DELETE',
+                        url: 'http://localhost:1501/api/book/' + bookId
+                    });
+                }
+            };
+        }
+    ]);
--- src/Bookshelf.Client/js/default.js
+++ src/Bookshelf.Client/js/default.js
@@ -30,5 +30,93 @@
         // args.setPromise().
     };
 
+    /**
+     * Our main app file with our module declarations and controllers.
+     *
+     * @author James Huston <james@jameshuston.net>
+     * @since 2013-07-18
+     */
+    angular.module('bookshelf', ['bookshelf.services.books'])
+        .config(function ($routeProvider) {
+            $routeProvider
+                .when('/book', {
+                    templateUrl: 'views/book.html',
+                    controller: 'BookController'
+                })
+                .when('/book/:bookId', {
+                    templateUrl: 'views/book.html',
+                    controller: 'BookController'
+                })
+                .when('/:filter', {
+                    templateUrl: 'views/books.html',
+                    controller: 'BookshelfController'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        })
+        .controller('BookshelfController', function ($scope, bookService, $routeParams) {
+            var filter = $routeParams.filter || undefined;
+            $scope.readFilter = {};
+            if (filter === 'reading') {
+                $scope.readFilter.Completed = 'false';
+            } else if (filter === 'completed') {
+                $scope.readFilter.Completed = 'true';
+            }
+
+            bookService
+                .getBooks()
+                .success(function (data, status, headers, config) {
+                    $scope.bookArray = data;
+                });
+
+            $scope.toggleStatus = function (book) {
+                book.Completed = !book.Completed;
+                bookService
+                    .updateBook(book.Id, book);
+            };
+
+            $scope.removeBook = function (bookId) {
+                bookService
+                    .deleteBook(bookId)
+                    .success(function () {
+                        bookService
+                            .getBooks()
+                            .success(function (data, status, headers, config) {
+                                $scope.bookArray = data;
+                            });
+                    });
+            };
+        })
+        .controller('BookController', function ($scope, bookService, $routeParams, $location) {
+            $scope.book = {};
+            $scope.bookId = $routeParams.bookId || undefined;
+            if ($scope.bookId !== undefined) {
+                bookService
+                    .getBooks($scope.bookId)
+                    .success(function (data, status, headers, config) {
+                        $scope.book = data;
+                    });
+            }
+
+            $scope.updateBook = function (bookId) {
+                if (bookId !== undefined) {
+                    bookService
+                        .updateBook(bookId, $scope.book);
+                } else {
+                    bookService
+                        .addBook($scope.book);
+                }
+                $location.url('/');
+            };
+
+            $scope.cancelUpdate = function () {
+                $location.url('/');
+            };
+        })
+        .controller('NavigationController', function ($scope, $location) {
+            $scope.path = $location.path();
+        });
+
     app.start();
 })();
